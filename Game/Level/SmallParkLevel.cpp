b067a48b22075a5343bcbe06688b9626dcfdf39b
yoonaward <yoonawardj@gmail.com>
2026-08-11 23:38:28 +0900

    테스트 맵 구현중

--- Game/Level/SmallParkLevel.cpp
+++ Game/Level/SmallParkLevel.cpp
@@ -1,13 +1,12 @@
 #include "SmallParkLevel.h"
 #include <Actor/Player.h>
+#include <iostream>
 
 using namespace Craft;
 
 SmallParkLevel::SmallParkLevel()
 {
 	
-
-	SpawnActor<Player>();
 }
 
 SmallParkLevel::~SmallParkLevel()
@@ -17,7 +16,13 @@ SmallParkLevel::~SmallParkLevel()
 
 void SmallParkLevel::BeginPlay()
 {
-	Level::BeginPlay();
+	super::BeginPlay();
+
+	// 플레이어 생성
+	std::shared_ptr<Player> player = SpawnActor<Player>();
+
+	// 안쪽에서 시작
+	player->SetPosition(Vector2(18, 3));
 }
 
 void SmallParkLevel::Tick(float deltaTime)
@@ -27,5 +32,15 @@ void SmallParkLevel::Tick(float deltaTime)
 
 void SmallParkLevel::Draw()
 {
+	// @Temp: 맵 출력.
+	for (int y = 0; y < 8; ++y)
+	{
+		std::cout << map[y] << "\n";
+	}
+
+
+	// 액터 그리기.
+
+
 	super::Draw();
 }
